ff4d1fb506e057ecfaa91997c06a6d489bb53637
Dvir Farkash <farkash@yatmal.co.il>
2023-06-11 19:41:13 +0300

    fixed auto-scrolling in different sccreen sizes

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,22 @@
+// auto scroll
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
-  async function timeSensativeAction(){ //must be async func
-    await sleep(185) //wait 5 seconds
-    window.scrollTo(0, 2000);
-}
-  function scrollDown(){
-    timeSensativeAction();
+  async function timeSensativeAction(element) {
+    await sleep(185); // wait 0.185 seconds
+    var rect = element.getBoundingClientRect();
+    var offset = window.pageYOffset || document.documentElement.scrollTop;
+    var targetScrollPosition = rect.top + offset - 200;
+    window.scrollTo({ top: targetScrollPosition, behavior: "smooth" });
+  }
+  
+  function scrollDown(event){
+    var element = event.target;
+    timeSensativeAction(element);
   }
 
 
+  // Copy Link button
   const copyLink = document.getElementById('copyLink');
   const originalText = copyLink.innerText;
 
@@ -42,4 +49,81 @@ const sleep = (milliseconds) => {
     copyLink.innerText = originalText;
   }, 1500); // 3000 milliseconds = 3 seconds
   });
-  
+
+//view options
+const viewOptions = document.querySelectorAll('.view-option');
+const contentContainer = document.getElementById('content');
+
+viewOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    // Remove the 'active' class from all view options
+    viewOptions.forEach((option) => {
+      option.classList.remove('active');
+    });
+
+    // Add the 'active' class to the clicked view option
+    option.classList.add('active');
+
+    // Get the data-view value of the clicked option
+    const view = option.dataset.view;
+
+    // Show the selected view and hide the others
+    const viewElements = contentContainer.children;
+    for (let i = 0; i < viewElements.length; i++) {
+      const element = viewElements[i];
+      if (element.classList.contains(view + '-view')) {
+        element.style.display = 'block';
+      } else {
+        element.style.display = 'none';
+      }
+    }
+  });
+});
+
+
+// attach auto-scroll to dropdowns based on screen size
+
+function attachClickEventBasedOnScreenSize() {
+    var elements = document.querySelectorAll("[data-bs-toggle='collapse']");
+    elements.forEach(function (element) {
+      var target = element.getAttribute("data-bs-target");
+      var collapseElement = document.querySelector(target);
+      if (collapseElement) {
+        var autoScrollClass = (collapseElement.classList.contains("auto-scroll-xs"))
+          ? "auto-scroll-xs"
+          : (collapseElement.classList.contains("auto-scroll-sm"))
+          ? "auto-scroll-sm"
+          : (collapseElement.classList.contains("auto-scroll-lg"))
+          ? "auto-scroll-lg"
+          : "";
+
+        if (autoScrollClass) {
+          var windowWidth = window.innerWidth;
+          if (
+            (autoScrollClass === "auto-scroll-xs" && windowWidth < 576) ||
+            (autoScrollClass === "auto-scroll-sm" && windowWidth < 992) ||
+            (autoScrollClass === "auto-scroll-lg")
+          ) {
+            element.addEventListener("click", scrollDown);
+          } else {
+            element.removeEventListener("click", scrollDown);
+          }
+        }
+      }
+    });
+  }
+
+  // Attach or detach click events based on the screen size initially
+  attachClickEventBasedOnScreenSize();
+
+  // Call attachClickEventBasedOnScreenSize whenever the window is resized
+  window.addEventListener("resize", attachClickEventBasedOnScreenSize);
+
+
+  /*
+  steps for auto-scroll based on screen size:
+  1. get the screen size
+  2. get all of the elements with "collpase"
+  3. get all of the elements that start with "auto-scroll-"
+
+  */
